c78beaabb646364a6fdb498e9f5f4cf8b462af03
unknownkhid <111437038+ShitaroKhido@users.noreply.github.com>
2022-12-04 09:25:43 +0700

    [TESTING]:

--- js/modules/site_functions.js
+++ js/modules/site_functions.js
@@ -146,7 +146,7 @@ const cardContainer = document.querySelector(".card-container");
 let productDataList = loadFromLocalStorage("productDataList");
 let userMode = ["customer", "editor"];
 // USE input here to change user views 1 for costumer 2 for seller:
-let input = 1;
+let input = 2;
 // RENDERING UI:
 // Rendering user interface to browser
 if (input === 1) {
